d13884eea8624071dc63bb20c5e9017983e353ca
MagomedIbr <magomedibragimov@yahoo.de>
2022-08-14 21:19:14 +0200

    Delete Button

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var selectedRow = null;
-function getFormData() {
+let getFormData=()=> {
     var formObj ={};
     formObj["Produkt"] = document.getElementById("Produkt").value;
     formObj["Preis"]=document.getElementById("Preis").value;
     formObj["Laden"]=document.getElementById("Laden").value;
     return formObj;
 }
-
+let rowCount = 0;
 //https://developer.mozilla.org/de/docs/Web/API/HTMLTableElement/insertRow
-function insertNewRecord(data){
+let insertNewRecord= (data)=>{
     var table = document.getElementById("Einkaufsliste");
     var newRow = table.insertRow(-1);
     cell1 = newRow.insertCell(0);
@@ -20,10 +20,18 @@ function insertNewRecord(data){
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = new Date().toUTCString();
     cell5 = newRow.insertCell(4);
-    cell5.innerHTML = `<button> Edit </button> <button> Delete </button>`
+    rowCount++;
+    cell5.innerHTML = `<button id = ${rowCount}> Edit </button> <button id =${rowCount} onclick="deleteM(this.id);"> Delete </button>`;
 }
 
-function formSubmit(e){
+let deleteM= (x)=> {
+    var table = document.getElementById("Einkaufsliste").getElementsByTagName('tr')[x];
+    document.getElementById("Einkaufsliste").getElementsByTagName('tr')[x].innerHTML = ``;
+    rowCount--;
+};
+
+
+let formSubmit=(e)=>{
     event.preventDefault();
         var formData = getFormData();
         if(selectedRow == null){
