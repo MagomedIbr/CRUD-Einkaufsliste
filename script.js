028ea56f1a64ae5b634a24fde4d2bc120f824019
MagomedIbr <magomedibragimov@yahoo.de>
2022-08-14 21:56:50 +0200

    edit mit contentEditable = true

--- script.js
+++ script.js
@@ -12,24 +12,35 @@ let insertNewRecord= (data)=>{
     var table = document.getElementById("Einkaufsliste");
     var newRow = table.insertRow(-1);
     cell1 = newRow.insertCell(0);
+    cell1.contentEditable = true;
     cell1.innerHTML = data.Produkt;
     cell2 = newRow.insertCell(1);
+    cell2.contentEditable = true;
     cell2.innerHTML = data.Preis;
     cell3 = newRow.insertCell(2);
+    cell3.contentEditable = true;
     cell3.innerHTML = data.Laden;
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = new Date().toUTCString();
     cell5 = newRow.insertCell(4);
     rowCount++;
-    cell5.innerHTML = `<button id = ${rowCount}> Edit </button> <button id =${rowCount} onclick="deleteM(this.id);"> Delete </button>`;
+    edit = "edit"+rowCount;
+    cell5.innerHTML = `<button id = ${edit} onclick="editM(this.id);"> Edit </button> <button id =${rowCount} onclick="deleteM(this.id);"> Delete </button>`;
 }
 
 let deleteM= (x)=> {
-    var table = document.getElementById("Einkaufsliste").getElementsByTagName('tr')[x];
+    var row = document.getElementById("Einkaufsliste").getElementsByTagName('tr')[x];
     document.getElementById("Einkaufsliste").getElementsByTagName('tr')[x].innerHTML = ``;
     rowCount--;
 };
 
+
+
+let editM= (x)=>{
+    const edit_button = document.getElementById(x);
+    edit_button.addEventListener("click", ()=> table.contentEditable =true)
+    
+}
 
 let formSubmit=(e)=>{
     event.preventDefault();
