ded7eeb492f27346b4d8065ee04e0baa2c49bc60
MagomedIbr <magomedibragimov@yahoo.de>
2022-08-15 20:24:33 +0200

    Bin sehr Nah dran...

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let getFormData=()=> {
 let rowCount = 0;
 //https://developer.mozilla.org/de/docs/Web/API/HTMLTableElement/insertRow
 let insertNewRecord= (data)=>{
+    rowCount++;
     var table = document.getElementById("Einkaufsliste");
     var newRow = table.insertRow(-1);
     cell1 = newRow.insertCell(0);
@@ -21,12 +22,11 @@ let insertNewRecord= (data)=>{
     cell2.innerHTML = data.Preis;
     cell3 = newRow.insertCell(2);
     cell3.contentEditable = true;
-    cell1.id = "Laden"+rowCount;
+    cell3.id = "Laden"+rowCount;
     cell3.innerHTML = data.Laden;
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = new Date().toUTCString();
     cell5 = newRow.insertCell(4);
-    rowCount++;
     edit = "edit"+rowCount;
     cell5.innerHTML = `<button id = ${edit} onclick="showEditField(this.id);"> Edit </button> <button id =${rowCount} onclick="deleteM(this.id);"> Delete </button>`;
 }
@@ -46,29 +46,38 @@ let formSubmit=(e)=>{
          }
 }
 
-let getEditData = () =>{
-    var editObj ={}
-    editObj("ProduktE")= document.getElementById("ProduktE").value;
-    editObj("PreisE") = document.getElementById("PreisE").value;
-    editObj("LadenE") = document.getElementById("LadenE").value;
+let getEditData = (e) =>{
+    event.preventDefault();
+    var editObj = {};
+    editObj["ProduktE"]= document.getElementById("ProduktE").value;
+    editObj["PreisE"] = document.getElementById("PreisE").value;
+    editObj["LadenE"] = document.getElementById("LadenE").value;
+    // console.log(editObj.ProduktE);
     return editObj;
 }
 
-let editRow = (x)=>{
+let editRows = (x)=>{
     x = x.slice(-1);
     var roww = document.getElementById("Einkaufsliste").getElementsByTagName('tr')[x];
-    console.log(roww);
+    console.log(x);
 }
 
-let edits = ()=>{
+editRow=null
+let edits = (e)=>{
     event.preventDefault();
+    x= editRow.slice(-1);
     var editData = getEditData();
+    console.log(editData.ProduktE);
+    const produktId = "Produkt"+x
+    const produktTemp = document.getElementById(produktId)
+    produktTemp.innerHTML = `<td contenteditable="true" id=&quot;`+ produktId  + `!&quot;>`+editData.ProduktE `</td>`
+    
 }
 
 
 //Zeigt das Edit Feld.
 let showEditField= (x)=>{
-    x = x.slice(-1);
+   
     var edit = document.getElementById("edit");
     if(edit.style.display==="none" || edit.style.display === null){
         edit.style.display="block";
@@ -76,6 +85,6 @@ let showEditField= (x)=>{
     else(
         edit.style.display="none"
     )
-    console.log(x);
-    return x;
+    editRow=x
+    // console.log(editRow)
 }
